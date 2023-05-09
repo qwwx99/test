86ed449997451eb05c9c0bc253b13672b7305dcb
qwwx99 <127303825+qwwx99@users.noreply.github.com>
2023-05-09 17:26:56 +0900

    서버에서 방 생성 처리

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const rooms = new Map();
+const rooms = [];
 
 // JSON 바디파서 미들웨어 등록
 app.use(bodyParser.json());
@@ -20,6 +20,21 @@ app.get("/", (req, res) => {
 
 app.get("/room", (req, res) => {
   res.sendFile(__dirname + "/views/room.html");
+});
+
+app.post("/room/create", (req, res) => {
+  const roomName = req.body.roomName;
+
+  // 방 생성 작업 수행
+  const newRoom = { id: rooms.length + 1, name: roomName };
+  rooms.push(newRoom);
+  console.log(rooms);
+
+  // 생성된 방 정보를 클라이언트에게 응답
+  res.json({
+    success: true,
+    roomId: newRoom.id,
+  });
 });
 
 var port = 3000;
--- public/javascript/client.js
+++ public/javascript/client.js
@@ -26,9 +26,33 @@ roomCreateButton.addEventListener("click", createRoom);
 
 function createRoom() {
   const roomName = document.getElementById("room-name").value;
-  // 방 생성 기능
-  alert(`새로운 ${roomName} 방을 생성합니다.`);
-  roomModal.style.display = "none"; // 모달창 닫기
+  // 새로운 목록 요소 생성
+
+  // 서버로 방 생성 요청 보내기
+  fetch("/room/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      roomName: roomName,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success) {
+        // 성공적으로 방이 생성된 경우
+        alert(`새로운 ${roomName} 방을 생성합니다.`);
+        window.location.href = `/room/${data.roomId}`;
+      } else {
+        // 방 생성에 실패한 경우
+        alert("방 생성에 실패했습니다.");
+      }
+    })
+    .catch((err) => console.error(err));
+
+  // 모달창 닫기
+  roomModal.style.display = "none";
   inputRoomName.value = "";
 }
 
